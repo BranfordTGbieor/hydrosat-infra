fdb2f7cea55f546d51e029fb9e55182413cbd5f3
Branford T Gbieor <gbieorbranford@gmail.com>
2026-04-03 13:17:48 +0200

    Add S3 access logging and multipart cleanup

--- terraform/modules/platform/main.tf
+++ terraform/modules/platform/main.tf
@@ -3,6 +3,94 @@ data "aws_kms_alias" "s3" {
   name  = "alias/aws/s3"
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "data_lake_logs" {
+  bucket = "${var.name_prefix}-data-lake-logs"
+
+  tags = merge(var.common_tags, {
+    Name      = "${var.name_prefix}-data-lake-logs"
+    Component = "platform"
+  })
+}
+
+resource "aws_s3_bucket_versioning" "data_lake_logs" {
+  bucket = aws_s3_bucket.data_lake_logs.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "data_lake_logs" {
+  bucket = aws_s3_bucket.data_lake_logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = var.enable_kms_hardening ? "aws:kms" : "AES256"
+      kms_master_key_id = var.enable_kms_hardening ? data.aws_kms_alias.s3[0].target_key_arn : null
+    }
+    bucket_key_enabled = var.enable_kms_hardening
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "data_lake_logs" {
+  bucket = aws_s3_bucket.data_lake_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "data_lake_logs" {
+  bucket = aws_s3_bucket.data_lake_logs.id
+
+  rule {
+    id     = "expire-noncurrent-log-versions"
+    status = "Enabled"
+
+    filter {
+      prefix = ""
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
+    }
+
+    noncurrent_version_expiration {
+      noncurrent_days = 30
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "data_lake_logs" {
+  bucket = aws_s3_bucket.data_lake_logs.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowS3ServerAccessLogs"
+        Effect = "Allow"
+        Principal = {
+          Service = "logging.s3.amazonaws.com"
+        }
+        Action   = "s3:PutObject"
+        Resource = "${aws_s3_bucket.data_lake_logs.arn}/data-lake-access-logs/*"
+        Condition = {
+          ArnLike = {
+            "aws:SourceArn" = aws_s3_bucket.data_lake.arn
+          }
+          StringEquals = {
+            "aws:SourceAccount" = data.aws_caller_identity.current.account_id
+          }
+        }
+      }
+    ]
+  })
+}
+
 resource "aws_s3_bucket" "data_lake" {
   bucket = "${var.name_prefix}-data-lake"
 
@@ -18,6 +106,12 @@ resource "aws_s3_bucket_versioning" "data_lake" {
   versioning_configuration {
     status = "Enabled"
   }
+}
+
+resource "aws_s3_bucket_logging" "data_lake" {
+  bucket        = aws_s3_bucket.data_lake.id
+  target_bucket = aws_s3_bucket.data_lake_logs.id
+  target_prefix = "data-lake-access-logs/"
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "data_lake" {
@@ -50,6 +144,10 @@ resource "aws_s3_bucket_lifecycle_configuration" "data_lake" {
 
     filter {
       prefix = ""
+    }
+
+    abort_incomplete_multipart_upload {
+      days_after_initiation = 7
     }
 
     transition {
